c22cc0a80ff4e23254068b3cf8c0cb783e085e3b
kundan-whitehat-kk <Kundan_kk52@outlook.com>
2017-09-16 23:22:38 +0530

    complete 1

--- app/src/main/java/code_by_kk_xample/com/pdfreader/MainActivity.java
+++ app/src/main/java/code_by_kk_xample/com/pdfreader/MainActivity.java
@@ -17,8 +17,13 @@ import android.widget.ImageView;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,15 +36,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pdfView = (PDFView)findViewById(R.id.pdfView);
+
+       // new RetrievePDFStream().execute("http://www.tutorialspoint.com/java/java_tutorial.pdf");
+        new RetrievePDFBytes().execute("http://iiti.ac.in/people/~tanimad/JavaTheCompleteReference.pdf");
     }
-    class RetrievePDFStream extends AsyncTask<String,Void,InputStream> {
+    class RetrievePDFBytes extends AsyncTask<String,Void,byte[]> {
 
         @Override
-        protected InputStream doInBackground(String... params) {
-            InputStream inputStream;
+        protected byte[] doInBackground(String... strings) {
+            InputStream inputStream = null;
             try {
-                URL url = new URL()
+                URL url = new URL(strings[0]);
+                HttpURLConnection URLConnection = (HttpURLConnection)url.openConnection();
+
+                if (URLConnection.getResponseCode() == 200) {
+                    inputStream = new BufferedInputStream(URLConnection.getInputStream());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
             }
+            try {
+                return IOUtils.toByteArray(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return new byte[0];
         }
+
+        @Override
+        protected void onPostExecute(byte[] bytes) {
+            pdfView.fromBytes(bytes).load();
+        }
+   /* class RetrievePDFStream extends AsyncTask<String,Void,InputStream> {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection URLConnection = (HttpURLConnection)url.openConnection();
+
+                if (URLConnection.getResponseCode() == 200) {
+                    inputStream = new BufferedInputStream(URLConnection.getInputStream());
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream).load();
+        } */
     }
 }
